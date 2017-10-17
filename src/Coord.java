75f0cc2f9afa8c779a019c24b3cfe04004b03d71
Amir <ashukaye@ualberta.ca>
2017-10-16 21:46:03 -0600

    Update Coord.java
    Fixed some stuff on Coord

--- src/Coord.java
+++ src/Coord.java
@@ -1,40 +1,53 @@
 import java.lang.IllegalArgumentException;
 
 public class Coord {
+
     public static int x;
     public static int y;
 
-    public Coord(int x, int y){
+    public Coord (int x, int y){
+
         this.x = x;
         this.y = y;
+
+    }
+
+    public Coord (){
+
     }
 
     public void setX(int x){
+        
         this.x = x;
+    
     }
 
     public void setY(int y){
+    
         this.y = y;
+    
     }
 
     public void setCoords(int x, int y){
+
         this.x = x;
         this.y = y;
+
     }
 
-    public void setCoords(String str) throws IllegalArgumentException{
+    public void setCoords(String str) {
+        
         char c = str.charAt(0);
 
+        // make sure input is 1 Letter (A-H or a=h) followed by 1 number(1-8)
+        if (str.length() == 2 && ( (c <= 'H' && c >= 'A') || (c < 'h' && c > 'a') ) &&  ('0' < str.charAt(1) && str.charAt(1) < '9') ) {
 
-        // make sure input is 1 Letter (A-H or a=h) followed by 1 number(1-8
-        if (str.length() == 2 && ((c < 73 && c > 64) || (c < 105 && c > 96) ) &&  ('0' < str.charAt(1) && str.charAt(1) < '9')  ) {
-
-            if (c < 73 && c > 64) {
+            if (c <= 'H' && c >= 'A') {
 
                 // char to int (0-7)
                 this.x = c - 'A';
 
-            } else if (c < 105 && c > 96) {
+            } else if (c <= 'h' && c >= 'a') {
 
                 // char to int (0 - 7)
                 this.x = c - 'a';
