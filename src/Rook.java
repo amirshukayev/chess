d5eec6942ea1933bc7f904aadd2c8dbacd274d39
Amir <ashukaye@ualberta.ca>
2017-11-08 17:42:32 -0700

    Create Rook.java

---
+++ src/Rook.java
@@ -0,0 +1,156 @@
+public class Rook extends Piece {
+
+    public Rook (Coord c, boolean white) {
+
+        super.white = white;
+        super.c = c;
+
+        if (white) {
+
+            super.letter = 'R';
+
+        } else {
+
+            super.letter = 'r';
+
+        }
+
+    }
+
+    public Rook (String s, boolean white) {
+
+        Coord c = new Coord(s);
+
+        super.white = white;
+        super.c = c;
+
+        if (white) {
+
+            super.letter = 'R';
+
+        } else {
+
+            super.letter = 'r';
+
+        }
+
+    }
+
+    public Rook (int x, int y, boolean white) {
+
+        super.white = white;
+
+        Coord c = new Coord(x, y);
+
+        super.c = c;
+
+        if (white) {
+
+            super.letter = 'R';
+
+        } else {
+
+            super.letter = 'r';
+
+        }
+
+    }
+
+    @Override
+    public MoveList getMoveList (Board b) {
+
+        MoveList moves = new MoveList();
+
+        int testX = super.c.x;
+        int testY = super.c.y;
+
+        testX--;
+
+        while (testX > -1) {
+
+            Coord c = new Coord(testX, testY);
+
+            if ( b.isSameColourPiece(c, super.white) ) {
+
+                break;
+
+            } else {
+
+                moves.addMove(super.c, c);
+
+            }
+
+            testX--;
+
+        }
+
+        testX = super.c.x;
+
+        testX++;
+        while (testX < 8) {
+
+            Coord c = new Coord(testX, testY);
+
+            if ( b.isSameColourPiece(c, super.white) ) {
+
+                break;
+
+            } else {
+
+                moves.addMove(super.c, c);
+
+            }
+
+            testX++;
+
+        }
+
+        testX = super.c.x;
+
+        testY--;
+        while (testY > -1) {
+
+            Coord c = new Coord(testX, testY);
+
+            if ( b.isSameColourPiece(c, super.white) ) {
+
+                break;
+
+            } else {
+
+                moves.addMove(super.c, c);
+
+            }
+
+            testY--;
+
+        }
+
+        testY = super.c.y;
+
+        testY++;
+        while (testY < 8){
+
+            Coord c = new Coord(testX, testY);
+
+            if ( b.isSameColourPiece(c, super.white) ) {
+
+                break;
+
+            } else {
+
+                Piece p = b.getPiece(c);
+
+                moves.addMove(super.c, c);
+
+            }
+
+            testY++;
+
+        }
+
+        return moves;
+
+    }
+
+}
