a200905ac968fa09a0c014b0acd5c0808f641546
Amir <ashukaye@ualberta.ca>
2017-11-09 10:48:57 -0700

    Update Knight.java
    This has the craziest glitch ever: moth moves returned are the same. Something might be static somewhere

--- src/Knight.java
+++ src/Knight.java
@@ -42,8 +42,49 @@ public class Knight extends Piece {
 
         MoveList m = new MoveList();
 
+        Coord c = new Coord(0,0);
+
         int testX = super.c.x;
         int testY = super.c.y;
+        testX+=2;
+        testY--;
+        c.setCoords(testX, testY);
+        if (testY > -1 && testX < 8 && !b.isSameColourPiece(c, super.white)) {
+            System.out.println("Worked 1");
+            m.addMove(super.c, c);
+        }
+
+
+
+        testX = super.c.x;
+        testY = super.c.y;
+        testX+=2;
+        testY++;
+        c.setCoords(testX, testY);
+        if (testY > -1 && testX < 8 && !b.isSameColourPiece(c, super.white)) {
+            System.out.println("Worked 2");
+            m.addMove(super.c, c);
+        }
+
+
+        testX = super.c.x;
+        testY = super.c.y;
+        testX--;
+        testY+=2;
+        c.setCoords(testX, testY);
+        if (testY > -1 && testX < 8 && !b.isSameColourPiece(c, super.white)) {
+            m.addMove(super.c, c);
+        }
+
+
+        testX = super.c.x;
+        testY = super.c.y;
+        testX++;
+        testY+=2;
+        c.setCoords(testX, testY);
+        if (testY > -1 && testX < 8 && !b.isSameColourPiece(c, super.white)) {
+            m.addMove(super.c, c);
+        }
 
         return m;
 
