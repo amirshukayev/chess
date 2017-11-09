160f75eb4069c14cd73b50ec31ca44bfc97a778b
Amir <ashukaye@ualberta.ca>
2017-11-08 17:49:29 -0700

    Create Main.java
    Right now this is just for testing stuff

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main (String[] args){
+
+        Board b = new Board();
+        b.resetBoard();
+
+        Piece p2 = new Rook("e5", true);
+        b.addPiece(p2);
+
+        b.printBoard();
+
+        Piece p = b.getPiece("e5");
+        MoveList m = p.getMoveList(b);
+
+        m.printMoves();
+
+        System.out.println("TODO: MAKE IT SO U CAN'T GO THROUGH BLACK PIECES FOR ROOK");
+
+    }
+
+}
