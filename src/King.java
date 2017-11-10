061751676f8a567f54baede030ab05fba657ed3d
Amir <ashukaye@ualberta.ca>
2017-11-10 16:37:43 -0700

    Update King.java

--- src/King.java
+++ src/King.java
@@ -17,6 +17,25 @@ public class King extends Piece {
 
     }
 
+    public King (String s, boolean white) {
+
+        Coord c = new Coord(s);
+
+        super.white = white;
+        super.c = c;
+
+        if (white) {
+
+            super.letter = 'K';
+
+        } else {
+
+            super.letter = 'k';
+
+        }
+
+    }
+
     public King (int x, int y, boolean white) {
 
         super.white = white;
@@ -44,6 +63,79 @@ public class King extends Piece {
 
         int testX = super.c.x;
         int testY = super.c.y;
+
+        if (testX != 0) {
+            testX--;
+            Coord c = new Coord(testX, testY);
+            m.addMove(super.c, c);
+        }
+
+        testX = super.c.x;
+        testY = super.c.y;
+
+        if (testX != 0 && testY != 0) {
+            testX--;
+            testY--;
+            Coord c = new Coord(testX, testY);
+            m.addMove(super.c, c);
+        }
+
+        testX = super.c.x;
+        testY = super.c.y;
+
+        if (testX != 0 && testY != 7) {
+            testX--;
+            testY++;
+            Coord c = new Coord(testX, testY);
+            m.addMove(super.c, c);
+        }
+
+        testX = super.c.x;
+        testY = super.c.y;
+
+        if (testY != 7) {
+            testY++;
+            Coord c = new Coord(testX, testY);
+            m.addMove(super.c, c);
+        }
+
+        testX = super.c.x;
+        testY = super.c.y;
+
+        if (testX != 7 && testY != 7) {
+            testX++;
+            testY++;
+            Coord c = new Coord(testX, testY);
+            m.addMove(super.c, c);
+        }
+
+        testX = super.c.x;
+        testY = super.c.y;
+
+        if (testX != 7) {
+            testX++;
+            Coord c = new Coord(testX, testY);
+            m.addMove(super.c, c);
+        }
+
+        testX = super.c.x;
+        testY = super.c.y;
+
+        if (testX != 7 && testY != 0) {
+            testX++;
+            testY--;
+            Coord c = new Coord(testX, testY);
+            m.addMove(super.c, c);
+        }
+
+        testX = super.c.x;
+        testY = super.c.y;
+
+        if (testY != 0) {
+            testY--;
+            Coord c = new Coord(testX, testY);
+            m.addMove(super.c, c);
+        }
 
         return m;
 
