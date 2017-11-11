a4f3254c79ef0a0f696098cd9f22558246b98da7
Amir <ashukaye@ualberta.ca>
2017-11-10 23:04:52 -0700

    Update Board.java

--- src/Board.java
+++ src/Board.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+
 public class Board extends Game {
 
     public Piece[][] board = new Piece[8][8];
+
+    ArrayList<Piece> pieces = new ArrayList();
 
     public Board () {
 
@@ -9,8 +13,69 @@ public class Board extends Game {
     public void addPiece (Piece p){
 
         board[p.c.x][p.c.y] = p;
+        pieces.add(p);
 
     }
+
+    public void addPiece (int x, int y, char a) {
+
+        Coord c = new Coord(x, y);
+
+        this.addPiece(c, a);
+
+    }
+
+
+    public void addPiece (String s, char a) {
+
+        Coord c = new Coord(s);
+
+        this.addPiece(c, a);
+
+    }
+
+
+    public void addPiece (Coord c, char a) {
+
+        if (a == 'n') {
+            Piece p = new Knight(c, false);
+            this.addPiece(p);
+        } else if (a == 'N') {
+            Piece p = new Knight(c, true);
+            this.addPiece(p);
+        } else if (a == 'b') {
+            Piece p = new Bishop(c, false);
+            this.addPiece(p);
+        } else if (a == 'B') {
+            Piece p = new Bishop(c, true);
+            this.addPiece(p);
+        } else if (a == 'p') {
+            Piece p = new Pawn(c, false);
+            this.addPiece(p);
+        } else if (a == 'P') {
+            Piece p = new Pawn(c, true);
+            this.addPiece(p);
+        } else if (a == 'q') {
+            Piece p = new Queen(c, false);
+            this.addPiece(p);
+        } else if (a == 'Q') {
+            Piece p = new Queen(c, true);
+            this.addPiece(p);
+        } else if (a == 'r') {
+            Piece p = new Rook(c, false);
+            this.addPiece(p);
+        } else if (a == 'R') {
+            Piece p = new Rook(c, true);
+            this.addPiece(p);
+        } else if (a == 'k') {
+            Piece p = new King(c, false);
+            this.addPiece(p);
+        } else if (a == 'K') {
+            Piece p = new King(c, true);
+            this.addPiece(p);
+        }
+    }
+
 
     // only do this if its legal
     public void move (Coord start, Coord end) {
@@ -68,13 +133,17 @@ public class Board extends Game {
         board[7][7] = new Rook(7, 7, false);
 
         for (int i = 0; i < 8; i++) {
-
             for (int j = 2; j < 6; j++) {
-
                 board[i][j] = new Empty(i, j);
-
             }
+        }
 
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (!this.isEmpty(i, j)) {
+                    pieces.add(board[i][j]);
+                }
+            }
         }
 
     }
@@ -154,6 +223,19 @@ public class Board extends Game {
             System.out.println();
 
         }
+
+    }
+
+    public MoveList allLegalMoves () {
+
+        MoveList m = new MoveList();
+
+        for (int i = 0; i < this.pieces.size(); i++) {
+            Piece p = this.pieces.get(i);
+            m.addAll(p.getMoveList(this));
+        }
+
+        return m;
 
     }
 
